eb3a163a79075462df4e4b2bccec4a8e37680062
Hpyer <me@hpyer.cn>
2020-12-24 11:22:35 +0800

    fix: parseWhereItem()

--- src/Support/Utils.ts
+++ src/Support/Utils.ts
@@ -100,9 +100,9 @@ export const xssFilter = function (str: string, whiteList: XSS.IWhiteList = {}):
 
 /**
  * 格式化时间
- * @param format 输出格式
- * @param datetime 时间对象、字符串等
- * @param offset 时间偏移量数值，正数表示增加，负数表示减少
+ * @param format 输出格式，默认：YYYY-MM-DD HH:mm:ss
+ * @param datetime 时间对象、字符串等，默认：null 表示当前时间
+ * @param offset 时间偏移量数值，正数表示增加，负数表示减少，默认：0 表示不做偏移
  * @param offsetUnit 时间偏移量单位，默认：'seconds'
  */
 export const getFormatTime = function (format: string = '', datetime: Moment.MomentInput = null, offset: number = 0, offsetUnit: Moment.unitOfTime.DurationConstructor = 'seconds'): string {
@@ -641,7 +641,7 @@ export const parseWhereItem = function(k: string, v: string | Array<string | boo
     return (is_and ? ' AND ' : ' OR ') + parseWhereValue(k, v as Array<string>);
   }
   else {
-    return ` AND \`${k}\`=${sqlEscape(v as string)}`;
+    return ` AND ${k}=${sqlEscape(v as string)}`;
   }
 };
 
